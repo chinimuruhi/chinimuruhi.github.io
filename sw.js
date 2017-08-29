e206f45cafa2004a71c6c68d9f7ed8c3b463215c
chinimuruhi <31010801+chinimuruhi@users.noreply.github.com>
2017-08-29 23:47:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const CACHE_NAME = 'cache';
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+    './',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+              .then((cache) => {
+                  console.log('Opened cache');
+
+                  // 指定されたリソースをキャッシュに追加する
+                  return cache.addAll(urlsToCache);
+              })
+    );
+});
+
 self.addEventListener('activate', (event) => {
     var cacheWhitelist = [CACHE_NAME];
 
@@ -15,6 +31,41 @@ self.addEventListener('activate', (event) => {
                 })
             );
         })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+              .then((response) => {
+                  if (response) {
+                      return response;
+                  }
+
+                  // 重要：リクエストを clone する。リクエストは Stream なので
+                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+                  // 必要なので、リクエストは clone しないといけない
+                  let fetchRequest = event.request.clone();
+
+                  return fetch(fetchRequest)
+                      .then((response) => {
+                          if (!response || response.status !== 200 || response.type !== 'basic') {
+                              return response;
+                          }
+
+                          // 重要：レスポンスを clone する。レスポンスは Stream で
+                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                          // 2つの Stream があるようにする
+                          let responseToCache = response.clone();
+
+                          caches.open(CACHE_NAME)
+                                .then((cache) => {
+                                    cache.put(event.request, responseToCache);
+                                });
+
+                          return response;
+                      });
+              })
     );
 });
 
